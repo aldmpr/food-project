905218dfc84647bbdfc087523eab040883ed38a4
Alex <aldmpr@gmail.com>
2022-08-22 10:34:22 +0200

    final?

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
@@ -5,13 +8,26 @@ import cards from './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
 import slider from './modules/slider';
+import {openModal} from './modules/modal';
 
 window.addEventListener("DOMContentLoaded", () => {
-    tabs();
-    modal();
-    timer();
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+    
+    tabs(".tabheader__item", ".tabcontent", ".tabheader__items", 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2022-09-11');
     cards();
     calc();
-    forms();
-    slider();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner',
+        slide: '.offer__slide'
+    });
 });
